fb8a310531202a73589ca3ad1c5ca00987599b3c
zy18722 <zy18722@nottingham.edu.cn>
2019-06-29 22:42:22 +0800

    “commit”

--- js/audio.js
+++ js/audio.js
@@ -15,15 +15,15 @@
 			.audio-hidden{width:0;height:0;visibility:hidden}\
 			.ppq-audio-player .play-pause-btn .play-pause-icon:after{position:absolute;top:50%;left:50%;-webkit-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);transform:translate(-50%,-50%)}\
 			.ppq-audio-player .play-pause-btn .play-pause-icon:after,.ppq-audio-player.player-playing .play-pause-icon:after{background-size:36px 18px}\
-			.ppq-audio-player{line-height:111px;position:relative;overflow:hidden;height:111px;margin:0 auto;background:#f4f4f4}\
+			.ppq-audio-player{line-height:80px;position:relative;overflow:hidden;height:80px;margin:0 auto;background:#f4f4f4}\
 			.ppq-audio-player audio{position:absolute;vertical-align:baseline}\
 			.ppq-audio-player .play-pause-btn{float:left;margin:29px 0 0 12px}\
 			.ppq-audio-player .play-pause-btn .play-pause-icon{position:relative;display:block;width:47px;height:47px;border:3px solid #00a293;border-radius:100%;background-color:#fff}\
 			.ppq-audio-player .play-pause-btn .play-pause-icon:after{display:block;content:"";background-position:0 0;width:17px;height:18px}\
 			.ppq-audio-player.player-playing .play-pause-icon:after{background-position:-25px 0;width:10px;height:18px}\
-			.ppq-audio-player .player-time{float:left;width:51px;margin-right:8px;text-align:right}\
+			.ppq-audio-player .player-time{float:left;width:50px;margin-right:8px;text-align:right}\
 			.ppq-audio-player .player-time-duration{float:right;margin:0 0 0 8px;text-align:left}\
-			.ppq-audio-player .player-bar{position:relative;overflow:hidden;height:5px;margin-top:53px;background-color:#fff}\
+			.ppq-audio-player .player-bar{position:relative;overflow:hidden;height:8px;margin-top:35px;background-color:#fff}\
 			.ppq-audio-player .player-bar .player-bar-loaded{position:absolute;top:0;left:0;width:100%;height:100%;border-radius:3px;background:#ddd}\
 			.ppq-audio-player .player-bar .player-bar-played{position:absolute;top:0;left:0;width:0;height:100%;border-radius:3px;background:#434445}\
 			</style>';
